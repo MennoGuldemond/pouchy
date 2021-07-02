642a055448d59a3f4d946615ddc169431287404f
Menno Guldemond <mennoguldemond@live.nl>
2021-07-02 22:41:24 +0200

    progress on chart for expenses

--- src/app/components/budget-overview/budget-overview.component.ts
+++ src/app/components/budget-overview/budget-overview.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { BudgetService } from '../../services';
-import { Entry, EntryType, Interval } from '../../models';
+import { Entry, EntryType, ExpenseCategory, Interval } from '../../models';
 import * as Calculate from '../../calculate';
 import { Router } from '@angular/router';
+import { ChartType } from 'angular-google-charts';
 
 @Component({
   selector: 'pchy-budget-overview',
@@ -11,12 +12,28 @@ import { Router } from '@angular/router';
 })
 export class BudgetOverviewComponent implements OnInit {
   entries: Entry[];
+  chart = null;
 
   constructor(private budgetService: BudgetService, private router: Router) {}
 
   ngOnInit(): void {
     this.budgetService.getEntriesForUser().subscribe((entries) => {
       this.entries = entries;
+
+      const expenses = this.entries.filter(e => e.type === EntryType.Expense);
+      const grouped = this.groupArrayOfObjects(expenses, 'category');
+      const data = [];
+      Object.keys(grouped).forEach(group => {
+        const total = Calculate.getTotalForInterval(grouped[group], Interval.Monthly);
+        data.push([ExpenseCategory[group], total]);
+      });
+
+      this.chart = {
+        type: ChartType.PieChart,
+        height: 500,
+        options: { animation: { duration: 1000, easing: 'out' }, is3D: false },
+        data: data,
+      };
     });
   }
 
@@ -37,4 +54,11 @@ export class BudgetOverviewComponent implements OnInit {
     );
     return Calculate.getTotalForInterval(expenseEntries, Interval.Monthly);
   }
+
+  groupArrayOfObjects(list, key) {
+    return list.reduce(function(rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  };
 }
